ecf1baf93ca29771182cd30a205d3b013711bfe1
venkateshMorishetti <117025754+venkateshMorishetti@users.noreply.github.com>
2023-12-01 09:18:55 +0530

    sidebar changes part 1

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,7 +1,35 @@
+import "./Sidebar.css";
 const Sidebar = () =>{
     return(
-        <div className="sidebar">
-            <h1>Sidebar</h1>
+        <div className="sidebar p-9 text-lg h-screen overflow-y-scroll"> 
+            <ul>
+                <li className="py-2">Home</li>
+                <li className="py-2">Shorts</li>
+                <li className="py-2">Subscriptions</li>
+            </ul>
+            <h1 className="py-2 header-size font-bold" >You</h1>
+            <ul>
+                <li className="py-2">Your channel</li>
+                <li className="py-2">History</li>
+                <li className="py-2">Your videos</li>
+                <li className="py-2">Watch Later</li>
+                <li className="py-2">Liked videos</li>
+            </ul>
+            <h1 className="py-2 header-size font-bold" >Subscriptions</h1>
+            <ul>
+                <li className="py-2">Aditya Music India</li>
+                <li className="py-2">WION</li>
+                <li className="py-2">T-series</li>
+                <li className="py-2">Think School</li>
+                <li className="py-2">Mango Music</li>
+                <li className="py-2">Entra idhi</li>
+                <li className="py-2">Aditya Music India</li>
+                <li className="py-2">WION</li>
+                <li className="py-2">T-series</li>
+                <li className="py-2">Think School</li>
+                <li className="py-2">Mango Music</li>
+                <li className="py-2">Entra idhi</li>
+            </ul>
         </div>
     )
 }
